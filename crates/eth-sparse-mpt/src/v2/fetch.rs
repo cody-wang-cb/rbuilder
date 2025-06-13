03c3bb1a4ce9883b53de85c8a311fe5b96b01e54
Cody Wang <cody.wang@coinbase.com>
2025-06-13 09:54:45 -0400

    update

--- crates/eth-sparse-mpt/src/v2/fetch.rs
+++ crates/eth-sparse-mpt/src/v2/fetch.rs
@@ -68,16 +68,12 @@ impl MissingNodesFetcher {
                         .map_err(SparseTrieError::other)?;
                     if !last_block_hash.is_zero() {
                         let block_number = provider
-                            .best_block_number()
+                            .last_block_number()
                             .map_err(SparseTrieError::other)?;
                         let block_hash = provider
                             .block_hash(block_number)
                             .map_err(SparseTrieError::other)?;
                         info!("(storage) block number: {:?}", block_number);
-                        let last_block_number = provider.last_block_number().map_err(SparseTrieError::other)?;
-                        let last_block_hash = provider.block_hash(last_block_number).map_err(SparseTrieError::other)?;
-                        info!("(storage) last block number: {:?}", last_block_number);
-                        info!("(storage) last block hash: {:?}", last_block_hash);
                         info!("(storage) block hash: {:?}", block_hash);
                         info!("(storage) shared cache last block hash: {:?}", shared_cache.last_block_hash);
                         if block_hash != Some(shared_cache.last_block_hash) {
@@ -114,13 +110,11 @@ impl MissingNodesFetcher {
             .map_err(SparseTrieError::other)?;
         if !last_block_hash.is_zero() {
             let block_number = provider
-                .best_block_number()
+                .last_block_number()
                 .map_err(SparseTrieError::other)?;
             let block_hash = provider
                 .block_hash(block_number)
                 .map_err(SparseTrieError::other)?;
-            info!("block hash: {:?}", block_hash);
-            info!("shared cache last block hash: {:?}", shared_cache.last_block_hash);
             if block_hash != Some(shared_cache.last_block_hash) {
                 return Err(SparseTrieError::WrongDatabaseTrieError);
             }
